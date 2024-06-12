5cf3b08a6da8890128499abd79563083c5b7aa4f
Ouail Derghal <ouailderghal@protonmail.com>
2024-06-12 15:00:54 +0200

    Add markdown settings to autocmd.lua

--- lua/ouailderghal/autocmd.lua
+++ lua/ouailderghal/autocmd.lua
@@ -20,4 +20,15 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
+-- Markdown settings
+local markdown_group = vim.api.nvim_create_augroup("markdown_settings", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	group = markdown_group,
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.textwidth = 90
+		vim.opt_local.colorcolumn = "90"
+	end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
