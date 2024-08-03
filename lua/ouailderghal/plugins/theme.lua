12a38281bf98830ec288bcd2e32d0f0b95db382d
Ouail Derghal <ouailderghal@protonmail.com>
2024-08-04 00:38:49 +0100

    Update colorscheme

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,11 +1,11 @@
+-- vim: ts=2 sts=2 sw=2 et
+
 return {
-	"ellisonleao/gruvbox.nvim",
+	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
 	cond = true,
 	config = function()
-		vim.cmd([[ colorscheme gruvbox ]])
+		vim.cmd([[ colorscheme tokyonight-night ]])
 	end,
 }
-
--- vim: ts=2 sts=2 sw=2 et
