b6f5d76f3c7aa103d5f985b0d14204db19fc7369
Ouail Derghal <ouailderghal@protonmail.com>
2024-09-04 16:36:52 +0200

    Refactor autocmd.lua

--- lua/ouailderghal/autocmd.lua
+++ lua/ouailderghal/autocmd.lua
@@ -1,4 +1,18 @@
--- Disable line numbers in terminal mode
+local function set_indent(tabstop, expandtab)
+	vim.bo.tabstop = tabstop
+	vim.bo.softtabstop = tabstop
+	vim.bo.shiftwidth = tabstop
+	vim.bo.expandtab = expandtab
+end
+
+local function set_textwidth(width)
+	vim.opt_local.textwidth = width
+	vim.opt_local.colorcolumn = tostring(width)
+end
+
+local indent_group = vim.api.nvim_create_augroup("indent_group", { clear = true })
+local textwidth_group = vim.api.nvim_create_augroup("textwidth_group", { clear = true })
+
 vim.api.nvim_create_autocmd("TermOpen", {
 	pattern = "*",
 	callback = function()
@@ -7,117 +21,61 @@ vim.api.nvim_create_autocmd("TermOpen", {
 	end,
 })
 
--- Go settings
-local go_group = vim.api.nvim_create_augroup("go_group", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-	group = go_group,
-	pattern = "go",
+	group = indent_group,
+	pattern = {
+		"go",
+		"yaml",
+		"ocaml",
+		"bash",
+		"make",
+		"dockerfile",
+		"lua",
+	},
 	callback = function()
-		vim.bo.tabstop = 2
-		vim.bo.softtabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.expandtab = true
+		set_indent(2, true)
 	end,
 })
 
--- YAML settings
-local yaml_group = vim.api.nvim_create_augroup("yaml_group", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-	group = yaml_group,
-	pattern = "yaml",
+	group = indent_group,
+	pattern = {
+		"typescript",
+		"javascript",
+		"python",
+	},
 	callback = function()
-		vim.bo.tabstop = 2
-		vim.bo.softtabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.expandtab = true
+		set_indent(4, true)
 	end,
 })
 
--- Make settings
-local make_group = vim.api.nvim_create_augroup("make_group", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-	group = make_group,
-	pattern = "make",
+	group = textwidth_group,
+	pattern = {
+		"go",
+		"yaml",
+		"ocaml",
+		"bash",
+		"lua",
+		"typescript",
+		"javascript",
+		"python",
+	},
 	callback = function()
-		vim.bo.tabstop = 2
-		vim.bo.softtabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.expandtab = false
+		set_textwidth(120)
 	end,
 })
 
--- Markdown settings
-local markdown_group = vim.api.nvim_create_augroup("markdown_group", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-	group = markdown_group,
-	pattern = "markdown",
+	group = textwidth_group,
+	pattern = {
+		"markdown",
+		"tex",
+		"org",
+		"dockerfile",
+		"make",
+	},
 	callback = function()
-		vim.opt_local.textwidth = 80
-		vim.opt_local.colorcolumn = "80"
+		set_textwidth(80)
 	end,
 })
-
--- OCaml settings
-local ocaml_group = vim.api.nvim_create_augroup("ocaml_group", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-	group = ocaml_group,
-	pattern = "ocaml",
-	callback = function()
-		vim.bo.tabstop = 2
-		vim.bo.softtabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.expandtab = true
-	end,
-})
-
--- Bash settings
-local bash_group = vim.api.nvim_create_augroup("bash_group", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-	group = bash_group,
-	pattern = "ocaml",
-	callback = function()
-		vim.bo.tabstop = 2
-		vim.bo.softtabstop = 2
-		vim.bo.shiftwidth = 2
-		vim.bo.expandtab = true
-	end,
-})
-
--- TypeScript settings
-local ts_group = vim.api.nvim_create_augroup("ts_group", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-	group = ts_group,
-	pattern = "typescript",
-	callback = function()
-		vim.bo.tabstop = 4
-		vim.bo.softtabstop = 4
-		vim.bo.shiftwidth = 4
-		vim.bo.expandtab = true
-	end,
-})
-
--- Javascript settings
-local js_group = vim.api.nvim_create_augroup("js_group", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-	group = js_group,
-	pattern = "javascript",
-	callback = function()
-		vim.bo.tabstop = 4
-		vim.bo.softtabstop = 4
-		vim.bo.shiftwidth = 4
-		vim.bo.expandtab = true
-	end,
-})
-
--- LaTeX settings
-local latex_group = vim.api.nvim_create_augroup("latex_group", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-	group = latex_group,
-	pattern = "tex",
-	callback = function()
-		vim.opt_local.textwidth = 80
-		vim.opt_local.colorcolumn = "80"
-	end,
-})
-
--- vim: ts=2 sts=2 sw=2 et
