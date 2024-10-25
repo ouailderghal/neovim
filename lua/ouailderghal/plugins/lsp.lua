c1721a1401ff7f70f89914d1a4f20044ea4a489d
Ouail Derghal <ouailderghal@protonmail.com>
2024-10-25 15:32:56 +0200

    Disable LSP autostart

--- lua/ouailderghal/plugins/lsp.lua
+++ lua/ouailderghal/plugins/lsp.lua
@@ -63,19 +63,42 @@ return {
 
 		-- LSP servers
 		local servers = {
-			gopls = {},
-			pyright = {},
-			clangd = {},
-			tsserver = {},
-			html = { filetypes = { "html" } },
-			ansiblels = {},
-			yamlls = {},
-			jsonls = {},
-			texlab = {},
-			bashls = {},
-			intelephense = {},
-
+			gopls = {
+        autostart = false,
+      },
+			pyright = {
+        autostart = false,
+      },
+			clangd = {
+        autostart = false,
+      },
+			tsserver = {
+        autostart = false,
+      },
+			html = { 
+        autostart = false,
+        filetypes = { "html" } 
+      },
+			ansiblels = {
+        autostart = false,
+      },
+			yamlls = {
+        autostart = false,
+      },
+			jsonls = {
+        autostart = false,
+      },
+			texlab = {
+        autostart = false,
+      },
+			bashls = {
+        autostart = false,
+      },
+			intelephense = {
+        autostart = false,
+      },
 			lua_ls = {
+        autostart = false,
 				settings = {
 					Lua = {
 						completion = {
@@ -108,5 +131,3 @@ return {
 		})
 	end,
 }
-
--- vim: ts=2 sts=2 sw=2 et
