2783a905954dd20b64a36c09f27163046482d675
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-09 22:22:22 +0100

    Add neotree.lua plugin

---
+++ lua/ouailderghal/plugins/neotree.lua
@@ -0,0 +1,16 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      {
+        "<leader>op",
+        "<cmd>Neotree toggle<cr>",
+        desc = "Toggle Neotree"
+      },
+    }
+}
