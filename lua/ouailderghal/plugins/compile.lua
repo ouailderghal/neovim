4479220a7e98389c035b5c4d274007973ff1e67d
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-09 20:47:52 +0100

    Add compile.lua plugin

---
+++ lua/ouailderghal/plugins/compile.lua
@@ -0,0 +1,13 @@
+return {
+  "ej-shafran/compile-mode.nvim",
+  tag = "v5.3.1",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "m00qek/baleia.nvim", tag = "v1.3.0" },
+  },
+  config = function()
+    vim.g.compile_mode = {
+      baleia_setup = true,
+    }
+  end
+}
