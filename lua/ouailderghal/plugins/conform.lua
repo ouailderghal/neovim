a9a2af0175a3171b2850e2a24720b1534668f200
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-09 22:32:01 +0100

    Add conform.lua plugin

---
+++ lua/ouailderghal/plugins/conform.lua
@@ -0,0 +1,23 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format { async = true, lsp_format = "fallback" }
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    notify_on_error = false,
+    format_on_save = false,
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+    },
+  },
+}
