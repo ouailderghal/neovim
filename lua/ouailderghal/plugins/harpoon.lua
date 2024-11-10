1c422f59edce69dffc8eaf06136e1d394919932d
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-10 19:36:53 +0100

    Update binding to mark files in harpoon.lua

--- lua/ouailderghal/plugins/harpoon.lua
+++ lua/ouailderghal/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>a", mark.add_file)
+		vim.keymap.set("n", "<leader>e", mark.add_file)
 		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
     vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
