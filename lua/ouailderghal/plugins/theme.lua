eb610d172b57af0a45da0d22d77ca35995446c80
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-12 14:47:39 +0100

    Override colors in theme.lua

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -3,7 +3,7 @@ return {
   name = "catppuccin",
   priority = 1000,
 
-  config = function() 
+  config = function()
     require("catppuccin").setup({
       flavour = "mocha",
       background = {
@@ -49,13 +49,38 @@ return {
         },
       },
       color_overrides = {
-        all = { text = "#ffffff" },
-        mocha = { base = "#000000" },
-    }
+        all = {
+          rosewater = "#ffffff",
+          flamingo = "#ffffff",
+          red = "#ffdd33",
+          maroon = "#ffffff",
+          pink = "#ffdd33",
+          mauve = "#ffdd33",
+          peach = "#96a6c8",
+          yellow = "#899b92",
+          green = "#73c936",
+          teal = "#88b992",
+          sky = "#cc8c3c",
+          sapphire = "#96a6c8",
+          blue = "#778899",
+          lavender = "#778899",
+          text = "#eae3d5",
+          subtext1 = "#d5c9b7",
+          subtext0 = "#bfb3a5",
+          overlay2 = "#aca195",
+          overlay1 = "#958b7e",
+          overlay0 = "#6f6660",
+          surface2 = "#585858",
+          surface1 = "#4b4b4b",
+          surface0 = "#353535",
+          base = "#181818",
+          mantle = "#1d2021",
+          crust = "#1d2021",
+        },
+      },
     })
 
     vim.opt.background = "dark"
-    vim.cmd.colorscheme "catppuccin"
-    -- vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#F38BA8" })
-  end
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
