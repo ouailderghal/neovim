fc74ccf100e95f0387efa60684f2f08fc78c4c85
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-12 15:34:44 +0100

    Disable lazy loading in vim-fugitive.lua

--- lua/ouailderghal/plugins/vim-fugitive.lua
+++ lua/ouailderghal/plugins/vim-fugitive.lua
@@ -1,18 +1,19 @@
 return {
-	"tpope/vim-fugitive",
-	cond = true,
+  "tpope/vim-fugitive",
+  cond = true,
+  lazy = false,
   keys = {
     {
-     "<leader>gg",
-     "<cmd>G<cr>"
+      "<leader>gg",
+      "<cmd>G<cr>",
     },
     {
       "<leader>gp",
-      "<cmd>G push<cr>"
+      "<cmd>G push<cr>",
     },
     {
       "<leader>gb",
-      "<cmd>!gh browse<cr>"
+      "<cmd>!gh browse<cr>",
     },
-  }
+  },
 }
