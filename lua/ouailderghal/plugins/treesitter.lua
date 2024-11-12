5272769beac8c8a2ccb0c53bd14e39adfa4deda6
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-12 14:00:18 +0100

    Update treesitter.lua
    * Move textobjects config to treesitter.lua
    * Add treesitter-context plugin

--- lua/ouailderghal/plugins/treesitter.lua
+++ lua/ouailderghal/plugins/treesitter.lua
@@ -1,53 +1,92 @@
 -- Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
--- Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
--- Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {
-		auto_install = false,
-		auto_sync = false,
-		additional_vim_regex_highlighting = true,
-		ensure_installed = {
-			"lua",
-			"luadoc",
-			"vim",
-			"vimdoc",
-			"diff",
-			"go",
-			"c",
-			"cpp",
-			"python",
-			"terraform",
-			"bash",
-			"markdown",
-			"javascript",
-			"typescript",
-			"latex",
-			"php",
-			"ocaml",
-			"dockerfile",
-			"java",
-			"json",
-			"yaml",
-		},
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = { 
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-context",
+  },
 
-		highlight = { enable = true },
-		indent = { enable = false },
-		textobjects = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "gnn",
-				node_incremental = "grn",
-				scope_incremental = "grc",
-				node_decremental = "grm",
-			},
-		},
-	},
+  build = ":TSUpdate",
 
-	config = function(_, opts)
-		require("nvim-treesitter.install").prefer_git = true
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+  opts = {
+    auto_install = false,
+    auto_sync = false,
+    additional_vim_regex_highlighting = true,
+    ensure_installed = {
+      "lua",
+      "luadoc",
+      "vim",
+      "vimdoc",
+      "diff",
+      "go",
+      "c",
+      "cpp",
+      "python",
+      "terraform",
+      "bash",
+      "markdown",
+      "javascript",
+      "typescript",
+      "latex",
+      "php",
+      "ocaml",
+      "dockerfile",
+      "java",
+      "json",
+      "yaml",
+    },
+
+    highlight = { enable = true },
+    indent = { enable = false },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
+
+    textobjects = {
+      enable = true,
+
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+      },
+
+      select = {
+        enable = true,
+        lookahead = true,
+
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = { query = "@class.inner" },
+          ["as"] = { query = "@local.scope", query_group = "locals" },
+        },
+
+        selection_modes = {
+          ["@parameter.outer"] = "v", -- charwise
+          ["@function.outer"] = "V", -- linewise
+          ["@class.outer"] = "<c-v>", -- blockwise
+        },
+
+        include_surrounding_whitespace = true,
+      },
+    },
+  },
+
+  config = function(_, opts)
+    require("nvim-treesitter.install").prefer_git = true
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
