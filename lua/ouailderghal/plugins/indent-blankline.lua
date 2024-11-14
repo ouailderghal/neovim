84e0cd4d77ac495ef83ef18b4df6faa0c5b902ee
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-14 21:09:41 +0100

    Lazy load indent-blankline.lua

--- lua/ouailderghal/plugins/indent-blankline.lua
+++ lua/ouailderghal/plugins/indent-blankline.lua
@@ -1,6 +1,11 @@
+local globals = require("ouailderghal.globals")
+
 return {
   "lukas-reineke/indent-blankline.nvim",
+
   cond = true,
+  lazy = true,
+  ft = globals.TREESITTER_ENSURE_INSTALLED,
   main = "ibl",
   opts = {},
 }
