f6da417832277a5be17fd03931e5b215c6f41bdc
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-14 21:08:40 +0100

    Lazy load nvim-treesitter.lua

--- lua/ouailderghal/plugins/nvim-treesitter.lua
+++ lua/ouailderghal/plugins/nvim-treesitter.lua
@@ -1,40 +1,22 @@
+local globals = require("ouailderghal.globals")
+
 return {
   "nvim-treesitter/nvim-treesitter",
   cond = true,
+  lazy = true,
+  build = ":TSUpdate",
+  ft = globals.TREESITTER_ENSURE_INSTALLED,
 
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-context",
   },
-
-  build = ":TSUpdate",
 
   opts = {
     auto_install = false,
     auto_sync = false,
     additional_vim_regex_highlighting = true,
-    ensure_installed = {
-      "lua",
-      "luadoc",
-      "vim",
-      "vimdoc",
-      "diff",
-      "go",
-      "c",
-      "cpp",
-      "python",
-      "terraform",
-      "bash",
-      "markdown",
-      "javascript",
-      "typescript",
-      "latex",
-      "php",
-      "ocaml",
-      "dockerfile",
-      "java",
-      "json",
-      "yaml",
-    },
+    ensure_installed = globals.TREESITTER_ENSURE_INSTALLED,
 
     highlight = { enable = true },
     indent = { enable = false },
@@ -88,5 +70,18 @@ return {
   config = function(_, opts)
     require("nvim-treesitter.install").prefer_git = true
     require("nvim-treesitter.configs").setup(opts)
+    require("treesitter-context").setup({
+      enable = true,
+      multiwindow = false,
+      max_lines = 0,
+      min_window_height = 0,
+      line_numbers = true,
+      multiline_threshold = 10,
+      trim_scope = "outer",
+      mode = "cursor",
+      separator = nil,
+      zindex = 10,
+      on_attach = nil,
+    })
   end,
 }
