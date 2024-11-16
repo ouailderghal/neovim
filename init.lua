e14ef4439e8a5666306283092422ee4a4eb55b46
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-16 13:09:25 +0100

    Refactor lazy.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+require("ouailderghal.options")
 require("ouailderghal.lazy")
 require("ouailderghal.groups")
 require("ouailderghal.keybindings")
