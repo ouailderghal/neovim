7e7d6b1bfd1656ab08942012544a8602de5c0af0
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-20 16:55:12 +0100

    Add onedark color scheme

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,33 +1,18 @@
 return {
-  "catppuccin/nvim",
-  cond = true,
-  name = "catppuccin",
-  priority = 1000,
+  {
+    "navarasu/onedark.nvim",
+    cond = true,
+    opt = {
+      style = "dark",
+      transparent = false,
+      term_colors = true,
+      ending_tildes = true,
+      cmp_itemkind_reverse = false,
 
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-      transparent_background = false,
-      show_end_of_buffer = true,
-      term_colors = false,
-      no_italic = false,
-      no_bold = false,
-      no_underline = false,
-      custom_highlights = {},
-      default_integrations = true,
+      toggle_style_key = nil,
+      toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" },
 
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-
-      dim_inactive = {
-        enabled = true,
-        shade = "dark",
-        percentage = 0.7,
-      },
-
-      styles = {
+      code_style = {
         comments = { "italic" },
         conditionals = { "bold" },
         loops = { "bold" },
@@ -42,51 +27,114 @@ return {
         operators = { "italic" },
       },
 
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
+      lualine = {
+        transparent = false,
+      },
+
+      colors = {},
+      highlights = {},
+
+      diagnostics = {
+        darker = true,
+        undercurl = true,
+        background = true,
+      },
+    },
+    config = function()
+      vim.opt.background = "dark"
+      vim.cmd.colorscheme("onedark")
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    cond = false,
+    name = "catppuccin",
+    priority = 1000,
+
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = false,
+        show_end_of_buffer = true,
+        term_colors = false,
+        no_italic = false,
+        no_bold = false,
+        no_underline = false,
+        custom_highlights = {},
+        default_integrations = true,
+
+        background = {
+          light = "latte",
+          dark = "mocha",
+        },
+
+        dim_inactive = {
           enabled = true,
-          indentscope_color = "",
+          shade = "dark",
+          percentage = 0.7,
         },
-      },
 
-      color_overrides = {
-        all = {
-          rosewater = "#ffffff",
-          flamingo = "#ffffff",
-          red = "#ffdd33",
-          maroon = "#ffffff",
-          pink = "#ffdd33",
-          mauve = "#ffdd33",
-          peach = "#96a6c8",
-          yellow = "#899b92",
-          green = "#73c936",
-          teal = "#88b992",
-          sky = "#cc8c3c",
-          sapphire = "#96a6c8",
-          blue = "#778899",
-          lavender = "#778899",
-          text = "#eae3d5",
-          subtext1 = "#d5c9b7",
-          subtext0 = "#bfb3a5",
-          overlay2 = "#aca195",
-          overlay1 = "#958b7e",
-          overlay0 = "#6f6660",
-          surface2 = "#585858",
-          surface1 = "#4b4b4b",
-          surface0 = "#353535",
-          base = "#181818",
-          mantle = "#1d2021",
-          crust = "#1d2021",
+        styles = {
+          comments = { "italic" },
+          conditionals = { "bold" },
+          loops = { "bold" },
+          functions = { "bold", "italic" },
+          keywords = {},
+          strings = { "italic" },
+          variables = {},
+          numbers = { "bold" },
+          booleans = { "bold", "underline" },
+          properties = {},
+          types = { "bold" },
+          operators = { "italic" },
         },
-      },
-    })
 
-    vim.opt.background = "dark"
-    vim.cmd.colorscheme("catppuccin")
-  end,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+        },
+
+        color_overrides = {
+          all = {
+            rosewater = "#ffffff",
+            flamingo = "#ffffff",
+            red = "#ffdd33",
+            maroon = "#ffffff",
+            pink = "#ffdd33",
+            mauve = "#ffdd33",
+            peach = "#96a6c8",
+            yellow = "#899b92",
+            green = "#73c936",
+            teal = "#88b992",
+            sky = "#cc8c3c",
+            sapphire = "#96a6c8",
+            blue = "#778899",
+            lavender = "#778899",
+            text = "#eae3d5",
+            subtext1 = "#d5c9b7",
+            subtext0 = "#bfb3a5",
+            overlay2 = "#aca195",
+            overlay1 = "#958b7e",
+            overlay0 = "#6f6660",
+            surface2 = "#585858",
+            surface1 = "#4b4b4b",
+            surface0 = "#353535",
+            base = "#181818",
+            mantle = "#1d2021",
+            crust = "#1d2021",
+          },
+        },
+      })
+
+      vim.opt.background = "dark"
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
