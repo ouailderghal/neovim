94b44e102560b4144e2627badf516a5e86e5f6fb
Ouail Derghal <ouailderghal@protonmail.com>
2024-11-26 16:59:47 +0100

    Add idea icon to todo-comments.lua

--- lua/ouailderghal/plugins/todo-comments.lua
+++ lua/ouailderghal/plugins/todo-comments.lua
@@ -6,7 +6,18 @@ return {
   lazy = true,
   ft = globals.CODE_FILE_TYPES,
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {},
+  opts = {
+    keywords = {
+      FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+      TODO = { icon = " ", color = "info" },
+      HACK = { icon = " ", color = "warning" },
+      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+      TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+      IDEA = { icon = "", color = "hint", alt = {} },
+    },
+  },
   keys = {
     {
       "[t",
