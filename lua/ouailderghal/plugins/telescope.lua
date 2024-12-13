980dc0d9e4c951ee4e50b142a8e48a5009b6625b
Ouail Derghal <ouailderghal@protonmail.com>
2024-12-13 12:47:31 +0100

    Update telescope.lua Add find_file and live_grep pickers config

--- lua/ouailderghal/plugins/telescope.lua
+++ lua/ouailderghal/plugins/telescope.lua
@@ -18,6 +18,18 @@ return {
 
   config = function()
     require("telescope").setup({
+      pickers = {
+        find_files = {
+          previewer = false,
+          theme = "ivy",
+        },
+
+        live_grep = {
+          previewer = true,
+          theme = "ivy",
+        },
+      },
+
       extensions = {
         ["ui-select"] = {
           require("telescope.themes").get_dropdown(),
