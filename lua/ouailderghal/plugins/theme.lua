6e7808eb73f1223ca29453bca3ed700e165b0008
Ouail Derghal <ouailderghal@protonmail.com>
2025-01-16 19:42:54 +0100

    Update theme

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,65 +1,39 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    cond = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true,
+  "navarasu/onedark.nvim",
+  cond = true,
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("onedark").setup({
+      style = "darker",
+      transparent = false,
+      term_colors = true,
+      ending_tildes = true,
+      cmp_itemkind_reverse = false,
+
+      toggle_style_key = nil,
+      toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" },
+
+      code_style = {
+        comments = "italic",
+        keywords = "none",
+        functions = "none",
+        strings = "none",
+        variables = "none",
+      },
+
+      colors = {},
+      highlights = {},
+      lualine = { transparent = false },
+
+      diagnostics = {
+        darker = true,
         undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true,
-        contrast = "hard",
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
+        background = true,
+      },
+    })
 
-      vim.opt.background = "dark"
-      vim.cmd.colorscheme("gruvbox")
-    end,
-  },
-  {
-    "miikanissi/modus-themes.nvim",
-    cond = true,
-    lazy = false,
-    priority = 1000,
-
-    config = function()
-      -- Default options
-      require("modus-themes").setup({
-        -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
-        -- `auto` will automatically set style based on background set with vim.o.background
-        style = "auto",
-        variant = "deuteranopia",
-        transparent = false,
-        dim_inactive = false,
-        hide_inactive_statusline = false,
-        styles = {
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-        },
-      })
-
-      vim.opt.background = "dark"
-      vim.cmd.colorscheme("modus")
-    end
-  },
+    vim.opt.background = "dark"
+    vim.cmd.colorscheme("onedark")
+  end,
 }
