f6046194d7f6683f7b86d4fa9b53d93bbd408847
Ouail Derghal <ouailderghal@protonmail.com>
2025-01-21 13:58:15 +0100

    Disable plugins/codeium.lua

--- lua/ouailderghal/plugins/codeium.lua
+++ lua/ouailderghal/plugins/codeium.lua
@@ -1,10 +1,12 @@
 return {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-    },
-    config = function()
-        require("codeium").setup({})
-    end
+  "Exafunction/codeium.nvim",
+  cond = false,
+
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    require("codeium").setup({})
+  end,
 }
