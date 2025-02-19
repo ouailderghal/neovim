5021cbc48fc717fd0471274e046215bcaa8750ea
Ouail Derghal <ouailderghal@protonmail.com>
2025-02-19 22:20:06 +0100

    Update theme

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,33 +1,9 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  "olimorris/onedarkpro.nvim",
   lazy = false,
+  priority = 1000,
   config = function()
-    require("gruvbox").setup({
-      terminal_colors = true,
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      invert_intend_guides = false,
-      inverse = true,
-      contrast = "",
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = false,
-    })
     vim.o.background = "dark"
-    vim.cmd("colorscheme gruvbox")
+    vim.cmd("colorscheme onedark_dark")
   end
-
 }
