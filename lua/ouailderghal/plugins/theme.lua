eef545eca549ce8b568f71b587fd2e4520377563
Ouail Derghal <ouailderghal@protonmail.com>
2025-03-31 20:55:33 +0200

    Update color scheme

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
-  "olimorris/onedarkpro.nvim",
+  "https://github.com/ellisonleao/gruvbox.nvim",
   lazy = false,
   priority = 1000,
   config = function()
     vim.o.background = "dark"
-    vim.cmd("colorscheme onedark_dark")
+    vim.cmd("colorscheme gruvbox")
   end
 }
