8c13fe2b7964db4298f3e64a08bed8a18f658e29
Ouail Derghal <ouailderghal@protonmail.com>
2025-05-12 11:02:15 +0200

    Update theme

--- lua/ouailderghal/init.lua
+++ lua/ouailderghal/init.lua
@@ -1,3 +1,4 @@
 require("ouailderghal.lazy")
 require("ouailderghal.options")
 require("ouailderghal.keybindings")
+vim.cmd("colorscheme evening")
--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,5 +1,6 @@
 return {
   "https://github.com/ellisonleao/gruvbox.nvim",
+  cond = false,
   lazy = false,
   priority = 1000,
   config = function()
