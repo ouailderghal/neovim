cf45b9db810d3eb41cbd3b05f13fc3fc4349a725
Ouail Derghal <ouailderghal@protonmail.com>
2025-08-25 15:53:40 +0200

    update theme

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,30 +1,63 @@
-return { 
-  "ellisonleao/gruvbox.nvim",
+return {
+  "rebelot/kanagawa.nvim",
   priority = 1000,
   config = function()
-      require("gruvbox").setup({
-      terminal_colors = true, -- add neovim terminal colors
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
+    require('kanagawa').setup({
+      compile = false,             -- enable compiling the colorscheme
+      undercurl = true,            -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true},
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false,         -- do not set background color
+      dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+      colors = {                   -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
       },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "", -- can be "hard", "soft" or empty string
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = false,
+      overrides = function(colors) -- add/modify highlights
+        return {}
+      end,
+      theme = "wave",              -- Load "wave" theme
+      background = {               -- map the value of 'background' option to a theme
+        dark = "wave",           -- try "dragon" !
+        light = "lotus"
+      },
     })
-    vim.cmd("colorscheme gruvbox")
-  end,
+
+    vim.cmd("colorscheme kanagawa")
+  end
 }
+
+-- return { 
+--   "ellisonleao/gruvbox.nvim",
+--   priority = 1000,
+--   config = function()
+--       require("gruvbox").setup({
+--       terminal_colors = true, -- add neovim terminal colors
+--       undercurl = true,
+--       underline = true,
+--       bold = true,
+--       italic = {
+--         strings = true,
+--         emphasis = true,
+--         comments = true,
+--         operators = false,
+--         folds = true,
+--       },
+--       strikethrough = true,
+--       invert_selection = false,
+--       invert_signs = false,
+--       invert_tabline = false,
+--       inverse = true, -- invert background for search, diffs, statuslines and errors
+--       contrast = "", -- can be "hard", "soft" or empty string
+--       palette_overrides = {},
+--       overrides = {},
+--       dim_inactive = false,
+--       transparent_mode = false,
+--     })
+--     vim.cmd("colorscheme gruvbox")
+--   end,
+-- }
