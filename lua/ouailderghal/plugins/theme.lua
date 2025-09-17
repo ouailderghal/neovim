6423ac5499818a96511ad4975e193bba6a98b8c8
Ouail Derghal <ouailderghal@protonmail.com>
2025-09-17 13:28:09 +0200

    update theme

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,30 +1,11 @@
-return { 
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000,
+return {
+  "navarasu/onedark.nvim",
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-      require("gruvbox").setup({
-      terminal_colors = true, -- add neovim terminal colors
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = "", -- can be "hard", "soft" or empty string
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = false,
-    })
-    vim.cmd("colorscheme gruvbox")
-  end,
+    require("onedark").setup {
+      style = "dark"
+    }
+    -- Enable theme
+    require("onedark").load()
+  end
 }
