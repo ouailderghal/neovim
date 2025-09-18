c8c4ce08527566b42bcc69295825ae95ad15dbe1
Ouail Derghal <ouailderghal@protonmail.com>
2025-09-18 12:03:22 +0200

    add onedark theme configuration

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,11 +1,37 @@
 return {
   "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
+  priority = 1000,
   config = function()
-    require("onedark").setup {
-      style = "dark"
-    }
-    -- Enable theme
+
+    require("onedark").setup({
+      style = 'dark',
+      transparent = false,
+      term_colors = true,
+      ending_tildes = false,
+      cmp_itemkind_reverse = false,
+
+      code_style = {
+          comments = 'italic',
+          keywords = 'none',
+          functions = 'bold',
+          strings = 'none',
+          variables = 'none'
+      },
+
+      lualine = {
+          transparent = false,
+      },
+
+      colors = {},
+      highlights = {},
+
+      diagnostics = {
+          darker = true,
+          undercurl = true,
+          background = true,
+      },
+    })
+
     require("onedark").load()
   end
 }
