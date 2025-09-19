7705086e56a071cd2da8e650e64a0008a1a7fcde
Ouail Derghal <ouailderghal@protonmail.com>
2025-09-19 17:20:21 +0200

    add neotree plugin

---
+++ lua/ouailderghal/plugins/neotree.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  }
+}
+
