3452e2a0ef79554106bb4207d01a718e6da0a9dc
Ouail Derghal <ouailderghal@protonmail.com>
2026-02-05 17:30:50 +0100

    update theme

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,65 +1,30 @@
-return {
-  "navarasu/onedark.nvim",
-  priority = 1000,
+return { 
+  "ellisonleao/gruvbox.nvim", 
+  priority = 1000 , 
   config = function()
-
-    require("onedark").setup({
-      style = "deep",
-      transparent = false,
-      term_colors = true,
-      ending_tildes = false,
-      cmp_itemkind_reverse = false,
-
-      code_style = {
-          comments = "italic",
-          keywords = "none",
-          functions = "bold",
-          strings = "none",
-          variables = "none"
-      },
-
-      lualine = {
-          transparent = false,
-      },
-
-      colors = {
-        black = "#151820",
-        bg0 = "#000000",
-        bg1 = "#2d3343",
-        bg2 = "#343e4f",
-        bg3 = "#363c51",
-        bg_d = "#1e242e",
-        bg_blue = "#6db9f7",
-        bg_yellow = "#f0d197",
-        fg = "#a5b0c5",
-        purple = "#ca72e4",
-        green = "#97ca72",
-        orange = "#d99a5e",
-        blue = "#5ab0f6",
-        yellow = "#ebc275",
-        cyan = "#4dbdcb",
-        red = "#ef5f6b",
-        grey = "#546178",
-        light_grey = "#7d899f",
-        dark_cyan = "#25747d",
-        dark_red = "#a13131",
-        dark_yellow = "#9a6b16",
-        dark_purple = "#8f36a9",
-        diff_add = "#303d27",
-        diff_delete = "#3c2729",
-        diff_change = "#18344c",
-        diff_text = "#265478",
-      },
-
-      highlights = {},
-
-      diagnostics = {
-          darker = true,
-          undercurl = true,
-          background = true,
-      },
+    require("gruvbox").setup({
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
     })
-
-    require("onedark").load()
-  end
+  vim.cmd("colorscheme gruvbox")
+  end, 
 }
