0ee8bdc04cf1665baa7f452e6c874285dcdb8cd5
Ouail Derghal <ouailderghal@protonmail.com>
2026-05-27 16:05:01 +0200

    add gruvbox theme

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,11 +1,6 @@
 local gh = require('config.util').gh
 
--- [[ Colorscheme ]]
--- You can easily change to a different colorscheme.
--- Change the name of the colorscheme plugin below, and then
--- change the command under that to load whatever the name of that colorscheme is.
---
--- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+-- [[ Tokyo Night ]]
 vim.pack.add { gh 'folke/tokyonight.nvim' }
 ---@diagnostic disable-next-line: missing-fields
 require('tokyonight').setup {
@@ -14,6 +9,17 @@ require('tokyonight').setup {
   },
 }
 
--- Like many other themes, this one has different styles, and you could load
--- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+-- [[ Gruvbox ]]
+vim.pack.add { gh 'ellisonleao/gruvbox.nvim' }
+---@diagnostic disable-next-line: missing-fields
+require('gruvbox').setup {
+  italic = {
+    strings = false,
+    emphasis = false,
+    comments = false,
+    operators = false,
+    folds = false,
+  },
+}
+
 vim.cmd.colorscheme 'tokyonight-night'
