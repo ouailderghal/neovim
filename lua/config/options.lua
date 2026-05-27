06a920ebf163fa06bc1ca976df167ad28634a1d3
Ouail Derghal <ouailderghal@protonmail.com>
2026-05-27 15:43:54 +0200

    set colorcolumn width

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -24,6 +24,7 @@ vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.o.inccommand = 'split'
 vim.o.cursorline = false
+vim.o.colorcolumn = '120'
 vim.o.scrolloff = 10
 vim.o.confirm = true
 
