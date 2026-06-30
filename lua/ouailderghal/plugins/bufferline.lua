5ad5b340b1de2c179cf4eeb1611f600662ef51ad
Ouail Derghal <ouailderghal@protonmail.com>
2026-06-30 17:31:39 +0200

    use minimal preset for bufferline

--- lua/ouailderghal/plugins/bufferline.lua
+++ lua/ouailderghal/plugins/bufferline.lua
@@ -3,9 +3,11 @@ return {
   version = "*",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("bufferline").setup({
+    local bufferline = require("bufferline")
+    bufferline.setup({
       options = {
         mode = "buffers",
+        style_preset = bufferline.style_preset.minimal,
         show_buffer_close_icons = false,
         show_close_icon = false,
         separator_style = "thin",
